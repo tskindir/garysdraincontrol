3e35e70014aa05acf8930e815c9762c764b0ecb3
Eskindir <tskindir@gmail.com>
2019-04-10 22:55:32 -0500

    build sidebar & routes

---
+++ garys/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './gdclogo.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+       <img src={logo} alt="Logo" width="95%"></img>
+      </div>
+    );
+  }
+}
+
+export default App;
