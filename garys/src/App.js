ce756a63007968b8f4b875757f7efc8554af2bd6
Eskindir <tskindir@gmail.com>
2019-04-30 16:16:13 -0500

    routes completed!

--- garys/src/App.js
+++ garys/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
+import Contact from "./pages/Contact/Contact.js";
+import Services from "./pages/Services/Services.js";
 import logo from './gdclogo.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <div className="App">
        
        <img src={logo} alt="Logo" width="95%"></img>
+       
       </div>
+      <Router>
+      <ul className="header">
+         <li><Link to="/">Home</Link></li>
+         <li><Link to="/Services">Services</Link></li>
+         <li><Link to="/Contact">Contact</Link></li>
+      </ul>
+        <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/Services/" component={Services}/>
+            <Route path="/Contact/" component={Contact}/>
+          </div>
+      </Router>
+      </div>
+
+     
     );
   }
 }
---
+++ garys/src/pages/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+
+render() {        
+    return ( 
+        <div>Contact Us!</div>
+    );
+}   
+}
+
+export default Contact;
